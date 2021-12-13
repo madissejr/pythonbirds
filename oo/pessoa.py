2eb9554107e312e2f4648694ae8492a1c4a4f6d1
madissejr <madissejr@github.com>
2021-12-13 14:50:00 +0200

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=26):
+    def __init__(self, *filhos, nome=None, idade=26):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Madisse')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Mendes'
-    print(p.nome)
-    print(p.idade)
+    mendes = Pessoa(nome='Mendes')
+    madisse = Pessoa(mendes, nome='Madisse')
+    print(Pessoa.cumprimentar(madisse))
+    print(id(madisse))
+    print(madisse.cumprimentar())
+    print(madisse.nome)
+    print(madisse.idade)
+    for filho in madisse.filhos:
+        print(filho.nome)
